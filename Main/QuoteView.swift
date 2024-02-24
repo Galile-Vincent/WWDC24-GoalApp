686d7b861e3d3dc58918fdb3e0f2f57675ef1139
Galile-Vincent <vincent.ua.248@gmail.com>
2024-02-24 13:18:31 +0800

    Update

--- Main/QuoteView.swift
+++ Main/QuoteView.swift
@@ -12,20 +12,24 @@ struct QuoteView: View {
     @State var user: UserData
     @State private var currentIndex = 0
     var body: some View {
-                Text("\"\(user.quotes[currentIndex])\"")
-                    .font(.system(.title3, design: .serif))
-                    .fontWeight(.regular) // Use appropriate font weight
-                    .foregroundColor(.black) // Choose an appropriate text color
-                    .multilineTextAlignment(.center) // Align the text to the center
-                    .padding() // Add padding around the text
-                    .cornerRadius(10) // Round the corners of the text container
-                    .onAppear {
-                // Start the timer when the view appears
-                Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { _ in
-                    // Update the index every minute
-                    self.updateIndex()
+        if currentIndex == 0{
+            
+        }else{
+            Text("\"\(user.quotes[currentIndex])\"")
+                .font(.system(.title3, design: .serif))
+                .fontWeight(.regular) // Use appropriate font weight
+                .foregroundColor(.black) // Choose an appropriate text color
+                .multilineTextAlignment(.center) // Align the text to the center
+                .padding() // Add padding around the text
+                .cornerRadius(10) // Round the corners of the text container
+                .onAppear {
+                    // Start the timer when the view appears
+                    Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { _ in
+                        // Update the index every minute
+                        self.updateIndex()
+                    }
                 }
-            }
+        }
     }
     func updateIndex() {
         // Get a random index
